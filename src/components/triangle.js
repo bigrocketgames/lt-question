e46c133c96778d2d4d1bf3c27acd91793593027b
Nathan Robbert <admin@bigrocketgames.com>
2018-06-07 14:44:29 -0700

    feat:  Triangle animation is working when clicking on the triangle.

--- src/components/triangle.js
+++ src/components/triangle.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+const removeAnimateClass = (el) => {
+  el.classList.remove("triangle-animate");
+}
+
+const handleClick = (e) => {
+  const el = document.getElementById(e.target.id);
+  el.classList.add("triangle-animate");
+
+  window.setTimeout(removeAnimateClass, 2000, el);
+}
+
 const Triangle = (props) => {
   return (
-    <div className="triangle-shape"></div>
+    <div id={`triangle${props.id}`} className="triangle-shape" onClick={handleClick}></div>
   )
 }
 
