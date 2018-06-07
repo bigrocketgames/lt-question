3c6cc5b55ee079c1a1db3842bcf2226185e094e5
Nathan Robbert <admin@bigrocketgames.com>
2018-06-07 14:54:57 -0700

    feat: Square animation works when clicking the shape.

--- src/components/square.js
+++ src/components/square.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+const removeAnimateClass = (el) => {
+  el.classList.remove("square-animate");
+}
+
+const handleClick = (e) => {
+  const el = document.getElementById(e.target.id);
+  el.classList.add("square-animate");
+
+  window.setTimeout(removeAnimateClass, 2000, el);
+}
+
 const Square = (props) => {
   return (
-    <div className="square-shape"></div>
+    <div id={`square${props.id}`} className="square-shape" onClick={handleClick}></div>
   )
 }
 
